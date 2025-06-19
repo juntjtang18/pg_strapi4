b786762c7ddc3b7178a700a46ff77d15b5160d6c
jamestjtang <juntjtang@163.com>
2025-06-18 23:27:51 -0700

    change the config/database.js point to strapi-db3. change the course to no publish needed.

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => {
     connection: {
       host: env('DATABASE_HOST', '/cloudsql/lucid-arch-451211-b0:us-west1:cloud-sql-server'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi-dev3'),
+      database: env('DATABASE_NAME', 'strapi-db3'),
       user: env('DATABASE_USERNAME', 'strapi'),
       password: env('DATABASE_PASSWORD', 'Passw0rd@Strapi'),
       schema: env('DATABASE_SCHEMA', 'public'),
